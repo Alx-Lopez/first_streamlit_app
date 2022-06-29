bfff6a29df62ef14f0368cc07ad4889a9c946eca
Alejandro Lopez <alexlopez1067@gmail.com>
2022-06-29 10:26:25 -0500

    fixed key error

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ import pandas
 file_path = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 fruit_list = pandas.read_csv(file_path)
 
-fruits_selected = streamlit.multiselect("Pick some fruits:",fruit_list,[fruit_list[0],fruit_list[1]])
+fruits_selected = streamlit.multiselect("Pick some fruits:",fruit_list,[str(fruit_list[0]),str(fruit_list[1])])
 
 filter_fruits = fruit_list.loc[fruits_selected]
 
