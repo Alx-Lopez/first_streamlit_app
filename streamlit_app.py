e9c0182b8fe9859af327ccc24949e947c9f4fc7b
Alejandro Lopez <alexlopez1067@gmail.com>
2022-06-29 10:30:47 -0500

    changed index to fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ import pandas
 
 file_path = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
 fruit_list = pandas.read_csv(file_path)
+fruit_list = fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(fruit_list.index),['Avocado','Strawberries'])
 
