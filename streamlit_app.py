daa7c7f382d933cb6bd26d43794b18d7c38023b0
Alejandro Lopez <alexlopez1067@gmail.com>
2022-07-06 13:26:10 -0500

    added get fruit function

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,17 @@ streamlit.dataframe(filter_fruits)
 streamlit.header('Fruityvice Fruit Advice!')
 streamlit.text(fruityvice_response.json())
 
+def get_fruit(fruit):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
+    fruit_normalized = pandas.json_normalize(fruityvice_response.json())
+    return streamlit.dataframe(fruit_normalized)
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruit_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruit_normalized)
+        get_fruit(fruit_choice)
 except URLError as e:
     streamlit.error()
 
